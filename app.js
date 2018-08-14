34e0b85bf5d9dfd3dff0898be88222bc084a5d53
eurowondollaryen <sehoakasayho@naver.com>
2018-08-14 21:40:48 +0900

    commit0814

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
-var http = require('http');
 var express = require('express');
+var app = express();
 var fs = require('fs');
 var path = require('path');
-var app = express();
+const port = process.env.PORT || 3000;
 
 var filename = 'index.html';
+
 app.use(express.static(path.join(__dirname, 'public')));
-app.all("*", (req, res, next) => {
-	res.writeHead(200, {'Content-Type' : 'text/html'});
-	next();
+
+app.listen(port, () => {
+	console.log('Server running on port 3000');
 });
 
 app.get("/", (req, res) => {
-	res.end(fs.readFileSync(filename, 'utf8'));
+	fs.readFile(filename, (err, data) => {
+		if(err) {
+			console.log(err);
+		}
+		else {
+			res.writeHead(200, {'Content-Type' : 'text/html'});
+			res.end(data);
+		}
+	});
 });
 /*
 app.get("*", (req, res) => {
 	res.end('<head><title>404</title></head><body><h1>404 Error</h1></body>');
 });
 */
-http.createServer(app).listen(3000, '127.0.0.1');
-console.log("Server running on 3000");
